4fe2ffd9206be40a7e84725375dbf305d7a87dd4
ramm037 <srirammahapatra29@gmail.com>
2026-07-25 12:02:46 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.45%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char> st;
+
+        for(int i = 0; i < str.size(); i++){
+            if(str[i] == '(' || str[i] == '{' || str[i] == '[') { //opening
+                st.push(str[i]);
+            } else {//closing
+                if(st.size() == 0){
+                    return false;
+                } 
+
+                if((st.top() == '(' && str[i] == ')') ||
+                   (st.top() == '{' && str[i] == '}') ||
+                   (st.top() == '[' && str[i] == ']')) {
+                    st.pop();
+                   } else { //no match
+                        return false;
+                }
+            }
+        }
+        return st.size() == 0;
+    }
+};
+
+//if the stack is empty that means no closing bracket can be used and if at the end the stack is not empty the opening brackets were more than the closing
