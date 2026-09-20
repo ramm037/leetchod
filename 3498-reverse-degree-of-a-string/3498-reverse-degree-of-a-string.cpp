accc54a1a539dafd5950cbdbe398812a279ae064
ramm037 <srirammahapatra03@gmail.com>
2026-09-20 19:22:10 +0530

    Time: 3 ms (16.14%), Space: 9.8 MB (47.88%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int reversePos = 'z' - s[i] + 1;
+            int stringPos = i + 1;
+
+            ans += reversePos * stringPos;
+        }
+
+        return ans;
+    }
+};
